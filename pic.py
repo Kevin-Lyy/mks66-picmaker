29f5e0f8124d0586cfdf1c145e473572264500d6
Kevinlyy <batmanthelegendly@gmail.com>
2019-01-31 20:42:57 -0500

    finished

--- pic.py
+++ pic.py
@@ -1,18 +1,18 @@
-filename = "file.ppm"
+filename = "Macrohard.ppm"
 file = open(filename,"w+")
 file.write("P3 500 500 255\n")
 
-for c in range(500):
-    for c in range(250):
-        file.write("255 75 20 ")
-
-for c2 in range(500):
-    for i2 in range(500):
-        file.write("235 176 40 ")
-
-#235 105 40
-#102 204 0
-#102 178 255
-#235 176 40
+for x in range(500):
+    for y in range(500):
+        if x < 250 and y < 250:
+            file.write("255 75 20 ")
+        elif x < 250 and y > 250:
+            file.write("102 204 0 ")
+        elif x > 250 and y < 250:
+            file.write("102 178 255 ")
+        elif x > 250 and y > 250:
+            file.write("235 176 40 ")
+        else:
+            file.write("0 0 0 ")
 
 file.close()
